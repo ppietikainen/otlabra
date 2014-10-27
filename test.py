fa754acc8b755e3e04a1243801431e5f6643d4c1
Jussi Virtanen <virtajus@st-cn0001.oulu.fi>
2014-10-27 14:36:36 +0200

    Feature: Range test for amount of fizzbuzzes

--- test.py
+++ test.py
@@ -13,10 +13,25 @@ class TestSuite(unittest.TestCase):
     def test_run(self):
         output = StringIO()
 
+        # Test if the amount of fizzbuzzes is correct in range 1-20
         app = FizzBuzz()
-        app.run(100, output)
+        app.run(20, output)
 
-        self.failIf(len(output.getvalue().splitlines()) != 100)
+        fizz_am = 5
+        buzz_am = 3
+        fizzbuzz_am = 1
+        fizz_am_test = buzz_am_test = fizzbuzz_am_test = 0
+        
+        for line in output.getvalue().splitlines():
+            if "FizzBuzz" in line:
+                fizzbuzz_am_test += 1
+            elif "Fizz" in line:
+                fizz_am_test += 1
+            elif "Buzz" in line:
+                buzz_am_test += 1
+
+        self.failIf(fizz_am_test != fizz_am or buzz_am_test != buzz_am \
+            or fizzbuzz_am_test != fizzbuzz_am)
 
 def main():
     unittest.main()
