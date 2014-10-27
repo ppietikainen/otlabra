4f84a4958b13332755f1048620f459ebc60e0d6e
Jussi Virtanen <virtajus@st-cn0001.oulu.fi>
2014-10-27 14:42:30 +0200

    Fix: calc for single value, return tuple with value and message

--- app.py
+++ app.py
@@ -18,18 +18,18 @@ class FizzBuzz():
     # Run from 1 to "end + 1". Version 1 functionality.
     def run(self, end, out=sys.stdout):
         for i in range(1, end + 1):
-            message = ""
-            if i % 3 == 0 and i % 5 == 0:
-                message = "FizzBuzz"
-            elif i % 3 == 0:
-                message = "Fizz"
-            elif i % 5 == 0:
-                message = "Buzz"
-            print >> out, self.calc(i), message
+            print >> out, self.calc(i)[0], self.calc(i)[1]
 
-    # Seems to give correct values. Tested with 1 and 2.
-    def calc(self, i):
-        return i
+    # Test for a single value
+    def calc(self, val):
+        message = ""
+        if val % 3 == 0 and val % 5 == 0:
+            message = "FizzBuzz"
+        elif val % 3 == 0:
+            message = "Fizz"
+        elif val % 5 == 0:
+            message = "Buzz"
+        return (val, message)
 
 if __name__ == "__main__":
     app = FizzBuzz()
