9b4bff78e13d754d7bf712124756aa0f4373d0f9
Karri Ojala <ojala.karri@gmail.com>
2014-10-27 13:28:30 +0200

    version 1 added

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python
 import sys
 
-# Implementation of FizzBuzz v0brackets python.00
+# Implementation of FizzBuzz v0.00
 
 # Version 1: if number is divisible by 3, print Fizz
 #            if number is divisible by 5, print Buzz
@@ -17,12 +17,20 @@ class FizzBuzz():
 
     # Run from 1 to "end". Maybe. Test fails for some reason
     def run(self, end, out=sys.stdout):
-        for i in range(1, end + 1):
-            print >> out, self.calc(i)
+        for i in range(1, end):
+            self.calc(i)
 
     # Seems to give correct values. Tested with 1 and 2.
     def calc(self, i):
-        return i
+        
+        if i % 3 == 0 and i % 5 == 0:
+            print "FizzBuzz"
+        elif i% 3 == 0:
+            print "Fizz"
+        elif i%5 == 0:
+            print "Buzz"
+        else: 
+            print i
 
 if __name__ == "__main__":
     app = FizzBuzz()
