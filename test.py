68b84276a11e7ed55150afdd9f5e6ed1de462a7c
Jiri Burysek <burysek.j@gmail.com>
2014-10-27 15:04:25 +0200

    added more tests

--- test.py
+++ test.py
@@ -17,6 +17,26 @@ class TestSuite(unittest.TestCase):
         app.run(100, output)
 
         self.failIf(len(output.getvalue().splitlines()) != 100)
+        
+    def test_hardcore(self):
+        output = StringIO()
+
+        app = FizzBuzz()
+        app.run(10000, output)
+
+        self.failIf(len(output.getvalue().splitlines()) != 10000)
+        
+    def test_primes(self):
+        app = FizzBuzz()
+        f = open('primes.txt', 'r')
+        for line in f:
+	        self.failIf(app.is_prime(int(line)) != True)
+
+    def test_nonprimes(self):
+        app = FizzBuzz()
+        f = open('nonprimes.txt', 'r')
+        for line in f:
+	        self.failIf(app.is_prime(int(line)) != False)
 
 def main():
     unittest.main()
