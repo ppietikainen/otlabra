3f0acab70d40ec3238b849bf613955e2a1cd6eb7
Karri Ojala <ojala.karri@gmail.com>
2014-10-27 14:17:59 +0200

    v2 toimii

--- app.py
+++ app.py
@@ -22,15 +22,27 @@ class FizzBuzz():
 
     # Seems to give correct values. Tested with 1 and 2.
     def calc(self, i):
-        
-        if i % 3 == 0 and i % 5 == 0:
-            return "FizzBuzz"
-        elif i% 3 == 0:
-            return "Fizz"
-        elif i%5 == 0:
-            return "Buzz"
-        else: 
+        isprime = True
+        if i == 1:
             return i
+        
+        else:
+            for j in range(2, i):
+                if (i%j == 0):
+                    isprime = False
+                
+            if isprime == True:
+                return "<%i> is a prime" % (i)
+            else:
+                
+                if i % 3 == 0 and i % 5 == 0:
+                    return "FizzBuzz"
+                elif i% 3 == 0:
+                    return "Fizz"
+                elif i%5 == 0:
+                    return "Buzz"
+                else:
+                    return i
 
 if __name__ == "__main__":
     app = FizzBuzz()
