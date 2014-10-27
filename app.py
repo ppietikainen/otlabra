045f6fd4183bceed003f7c4fdcfc9889bc288fb6
Jussi Virtanen <virtajus@el-cn0001.oulu.fi>
2014-10-27 13:04:46 +0200

    Feature: Version 1 functionality

--- app.py
+++ app.py
@@ -15,10 +15,16 @@ class FizzBuzz():
     def __init__(self):
         pass
 
-    # Run from 1 to "end + 1".
+    # Run from 1 to "end + 1". Version 1 functionality.
     def run(self, end, out=sys.stdout):
         for i in range(1, end + 1):
             print >> out, self.calc(i)
+            if i % 3 == 0 and i % 5 == 0:
+                print "FizzBuzz"
+            elif i % 3 == 0:
+                print "Fizz"
+            elif i % 5 == 0:
+                print "Buzz"
 
     # Seems to give correct values. Tested with 1 and 2.
     def calc(self, i):
