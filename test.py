a6678a6835ce3b172b7246684b0c9a254b75ceee
mihannil <mhannila@hotmail.com>
2014-10-27 14:14:40 +0200

    Upgraded test.py
    Added tests to test.py.

--- test.py
+++ test.py
@@ -18,6 +18,14 @@ class TestSuite(unittest.TestCase):
 
         self.failIf(len(output.getvalue().splitlines()) != 100)
 
+    def test_ver1(self):
+        app = FizzBuzz()
+        self.failIf(app.calc(87) != "Fizz")
+        self.failIf(app.calc(20) != "Buzz")
+        self.failIf(app.calc(45) != "FizzBuzz")
+        self.failIf(app.calc(1500) != "FizzBuzz")
+        self.failIf(app.calc(37284) != "Fizz")
+
 def main():
     unittest.main()
 
