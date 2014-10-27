11da8aadcdce99a40bf509c88ffd6585276466a3
Jussi Virtanen <virtajus@st-cn0001.oulu.fi>
2014-10-27 15:02:24 +0200

    Feature: Unit test for value of one and negative value. Test for run function fixed for new output

--- test.py
+++ test.py
@@ -8,7 +8,11 @@ class TestSuite(unittest.TestCase):
 
     def test_one(self):
         app = FizzBuzz()
-        self.failIf(app.calc(1) != 1)
+        self.failIf(app.calc(1) != (1, ""))
+
+    def test_negative(self):
+        app = FizzBuzz()
+        self.failIf(app.calc(-1000) != (-1000, ""))
 
     def test_run(self):
         output = StringIO()
@@ -16,7 +20,13 @@ class TestSuite(unittest.TestCase):
         app = FizzBuzz()
         app.run(1000, output)
 
-        self.failIf(len(output.getvalue().splitlines()) != 168)
+        prime_amount = 0
+
+        for line in output.getvalue().splitlines():
+            if "Is prime!" in line:
+                prime_amount += 1
+
+        self.failIf(prime_amount != 168)
 
 def main():
     unittest.main()
