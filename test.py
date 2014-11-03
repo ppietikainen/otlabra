281541e377afeb01e0e9ec77e050a2201149a905
Erkka Makinen <ermakine@st-cn0001.oulu.fi>
2014-11-03 12:53:54 +0200

    LisättyFizzBuzz tuloste ja pari unit testiä

--- test.py
+++ test.py
@@ -10,13 +10,40 @@ class TestSuite(unittest.TestCase):
         app = FizzBuzz()
         self.failIf(app.calc(1) != 1)
 
+    def test_three(self):
+	app = FizzBuzz()
+	self.failIf(app.calc(3) != "Fizz")
+
+    def test_five(self):
+	app = FizzBuzz()
+	self.failIf(app.calc(5) != "Buzz")
+
+    def test_fifteen(self):
+	app = FizzBuzz()
+	self.failIf(app.calc(15) != "FizzBuzz")
+
     def test_run(self):
         output = StringIO()
 
         app = FizzBuzz()
         app.run(100, output)
 
+	fizz = 0
+	buzz = 0
+	fizzbuzz = 0
+
+	for i in output.getvalue():
+	    if i == "Fizz":
+		fizz += 1
+	    elif i == "Buzz":
+		buzz += 1
+	    elif i == "FizzBuzz":
+		fizzbuzz += 1
+
         self.failIf(len(output.getvalue().splitlines()) != 100)
+	self.failIf(fizzbuzz != 6)
+	self.failIf(fizz != 27)
+	self.failIf(buzz != 14)
 
 def main():
     unittest.main()
